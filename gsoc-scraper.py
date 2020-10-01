996f6ff7b1f4942555e881eb2603a6b8582695c7
nagasai-iitr <40641467+nagasai-iitr@users.noreply.github.com>
2020-10-01 16:39:50 +0530

    Update gsoc-scraper.py
    Customised the code for ease to understand and fixed the errors

--- gsoc-scraper.py
+++ gsoc-scraper.py
@@ -13,21 +13,21 @@ for year in years:
         orgs_list.append(org.text.strip())
 
     for org in orgs_list:
-        add=int(year)-2015
+        add=int(year)-2019
         add*=add
-        if(org in orgs):orgs[org]+=add
+        if(og in orgs):orgs[og]+=add
         else:orgs[org]=add
 #print(orgs)
-orgs_sorted=sorted(orgs.items(), key=lambda kv: kv[1],reverse=True)
-for org in orgs_sorted:
-    val=org[1]
-    if(org[1]==14):
-        print(org[0],"2018",2017,2016)
-    elif(val==13):print(org[0],2018,2017)
-    elif(val==9):print(org[0],2018)
-    elif(val==10):print(org[0],2018,2016)
-    elif(val==4):print(org[0],2017)
-    elif(val==1):print(org[0],2016)
-    elif(val==5):print(org[0],2017,2016)
+orgs_sorted=sorted(orgs.items(),reverse=True, key=lambda kv: kv[1])
+for og in orgs_sorted:
+    val=og[1]
+    if(og[1]==14):
+        print(og[0],"2019",2017,2016)
+    elif(val==13):print(og[0],2019,2017)
+    elif(val==9):print(og[0],2019)
+    elif(val==10):print(og[0],2019,2016)
+    elif(val==4):print(og[0],2017)
+    elif(val==1):print(og[0],2016)
+    elif(val==5):print(og[0],2019,2016)
     
 
